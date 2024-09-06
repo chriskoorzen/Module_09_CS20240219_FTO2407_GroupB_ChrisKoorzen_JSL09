71c7094d3d3a22acdfdc79ff88acec938e399cea
Chris Koorzen <fckoorzen@gmail.com>
2024-09-06 12:59:26 +0200

    Implement weather widget.

--- index.js
+++ index.js
@@ -45,7 +45,52 @@ if (navigator.geolocation){                             // Does browser support 
                 .then(res => res.json())
                 // catch non-200 HTTP responses
                 .then(res => {
-                    console.log("weather", res);
+
+                    // Main weather widget creation
+                    const weather_content = `
+                    <div class="flex flex-row justify-between items-center">
+                        <div class="mr-8">
+                            <p class="text-3xl">${res.name}</p>
+                            ${res.weather.map(obj => "<p>"+obj.description+"</p>").join('')}
+                        </div>
+                        <div class="">
+                            <p>icon</p>
+                            <p>${res.main.temp}&deg;C</p>
+                        </div>
+                    </div>
+                    <hr class="my-3">
+                    <div id="weather-extra" class="hidden">
+                        <div>
+                            <p>Feels like ${res.main.feels_like}&deg;C</p>
+                            <p>Max ${Math.round(res.main.temp_max)}&deg;C Min ${Math.round(res.main.temp_min)}&deg;C</p>
+                            <p>Humidity: ${res.main.humidity}%</p>
+                            <p>Cloud cover: ${res.clouds.all}%</p>
+                        </div>
+                        <hr class="my-3">
+                        <div class="flex flex-row justify-between">
+                            <div>
+                                <p>Wind</p>
+                                <p>${res.wind.speed} km/h</p>
+                                <p>Gusts of ${res.wind.gust} km/h</p>
+                            </div>
+                            <div class="rounded-full bg-stone-600 size-20">${res.wind.deg}</div>
+                        </div>
+                        <hr class="my-3">
+                        <div class="flex flex-row justify-between">
+                            <p>rise: ${ (new Date(res.sys.sunrise*1000)).toLocaleString('en-us', {timeStyle: "short"}) }</p>
+                            <p>set: ${ (new Date(res.sys.sunset*1000)).toLocaleString('en-us', {timeStyle: "short"}) }</p>
+                        </div>
+                    </div>`;
+
+                    // Add to document
+                    const weather = document.getElementById("weather");
+                    weather.innerHTML = weather_content;
+
+                    // Register event listener to toggle 'weather-extra'
+                    weather.addEventListener("click", () => {
+                        document.getElementById("weather-extra").classList.toggle("hidden");
+                    });
+
                 })
                 .catch(error => {
                     alert("Something broke. Check console");
