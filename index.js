2197aa7bca86c67177e95afa21a12cf1b108d460
Chris Koorzen <fckoorzen@gmail.com>
2024-09-18 18:19:05 +0200

    Refactor coordinate data returns into Promise object.

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 "use strict";
 import { OpenWeatherKey } from "./keys.js";
 
-const gcsKey = "w_coords";          // (geographic coordinate system) key for localStorage data
-
 
 // --- BACKGROUND ---
 // Get a random background image from remote location
@@ -44,212 +42,209 @@ function updateTime(){
 
 
 // --- WEATHER ---
-// Weather Display
-function updateWeather(){
-    
-    if (localStorage.getItem(gcsKey) === null){
-        getCoordinates();
-    }
-    else {
-        const c = JSON.parse(localStorage.getItem(gcsKey));
-        
-        fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${c.latitude}&lon=${c.longitude}&appid=${OpenWeatherKey}&units=metric`)
-        .then(response => {
-            if (response.ok) { return response.json(); }
-            else { console.log(response); throw Error("OpenWeatherMap API failed");}
-        })
-        .then(data => {
+const gcsKey = "w_coords";          // (geographic coordinate system) key for localStorage data
 
-            // Main weather widget creation
-            const weather_content = `
-            <div class="flex flex-row justify-between items-center">
-                <div class="mr-8">
-                    <p class="text-3xl">${data.name}</p>
-                    ${data.weather.map(obj => '<p class="capitalize">'+obj.description+"</p>").join('')}
+// Weather Display
+async function updateWeather(){
+
+    const c = await getCoordinates();
+
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${c.latitude}&lon=${c.longitude}&appid=${OpenWeatherKey}&units=metric`)
+    .then(response => {
+        if (response.ok) { return response.json(); }
+        else { console.log(response); throw Error("OpenWeatherMap API failed");}
+    })
+    .then(data => {
+
+        // Main weather widget creation
+        const weather_content = `
+        <div class="flex flex-row justify-between items-center">
+            <div class="mr-8">
+                <p class="text-3xl">${data.name}</p>
+                ${data.weather.map(obj => '<p class="capitalize">'+obj.description+"</p>").join('')}
+            </div>
+            <div class="">
+                <img class="inline" src="https://openweathermap.org/img/wn/${data.weather[0].icon}.png"/>
+                <p>${data.main.temp}&deg;C</p>
+            </div>
+        </div>
+        <hr class="my-3">
+        <div id="weather-extra" class="hidden">
+            <div>
+                <p>Feels like ${data.main.feels_like}&deg;C</p>
+                <p>Max ${Math.round(data.main.temp_max)}&deg;C Min ${Math.round(data.main.temp_min)}&deg;C</p>
+                <p>Humidity: ${data.main.humidity}%</p>
+                <p>Cloud cover: ${data.clouds.all}%</p>
+            </div>
+            <hr class="my-3">
+            <div class="flex flex-row justify-between">
+                <div>
+                    <p>Wind from ${
+                        (data.wind.deg > 337 || data.wind.deg < 23) ? "N":
+                        (data.wind.deg >= 23 && data.wind.deg < 68) ? "NE":
+                        (data.wind.deg >= 68 && data.wind.deg < 113) ? "E":
+                        (data.wind.deg >= 113 && data.wind.deg < 158) ? "SE":
+                        (data.wind.deg >= 158 && data.wind.deg < 203) ? "S":
+                        (data.wind.deg >= 203 && data.wind.deg < 248) ? "SW":
+                        (data.wind.deg >= 248 && data.wind.deg < 293) ? "W":
+                        "NW"
+                    } direction</p>
+                    <p>${data.wind.speed} km/h</p>
+                    ${ data.wind.gust ? "<p>Gusts of " + data.wind.gust + "km/h</p>": "" }
                 </div>
-                <div class="">
-                    <img class="inline" src="https://openweathermap.org/img/wn/${data.weather[0].icon}.png"/>
-                    <p>${data.main.temp}&deg;C</p>
+                <div class="rounded-full bg-stone-600 size-20">
+                    <svg class="h-16 mx-auto mt-2" style="rotate:${180+data.wind.deg}deg;" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 40 210" preserveAspectRatio="xMidYMid meet" >
+                        <g fill="none" stroke="orange" stroke-width="5" stroke-linejoin="round">
+                            <path d="M 20 10 V 100" stroke-dasharray="5,6"></path>
+                            <path d="M 20 100 V 200"></path>
+                            <path d="M 0 40 L 20 10 L 40 40"></path>
+                            <path d="M 0 180 L 20 150 L 40 180"></path>
+                            <path d="M 0 200 L 20 170 L 40 200"></path>
+                        </g>
+                    </svg>
                 </div>
             </div>
             <hr class="my-3">
-            <div id="weather-extra" class="hidden">
-                <div>
-                    <p>Feels like ${data.main.feels_like}&deg;C</p>
-                    <p>Max ${Math.round(data.main.temp_max)}&deg;C Min ${Math.round(data.main.temp_min)}&deg;C</p>
-                    <p>Humidity: ${data.main.humidity}%</p>
-                    <p>Cloud cover: ${data.clouds.all}%</p>
-                </div>
-                <hr class="my-3">
-                <div class="flex flex-row justify-between">
-                    <div>
-                        <p>Wind from ${
-                            (data.wind.deg > 337 || data.wind.deg < 23) ? "N":
-                            (data.wind.deg >= 23 && data.wind.deg < 68) ? "NE":
-                            (data.wind.deg >= 68 && data.wind.deg < 113) ? "E":
-                            (data.wind.deg >= 113 && data.wind.deg < 158) ? "SE":
-                            (data.wind.deg >= 158 && data.wind.deg < 203) ? "S":
-                            (data.wind.deg >= 203 && data.wind.deg < 248) ? "SW":
-                            (data.wind.deg >= 248 && data.wind.deg < 293) ? "W":
-                            "NW"
-                        } direction</p>
-                        <p>${data.wind.speed} km/h</p>
-                        ${ data.wind.gust ? "<p>Gusts of " + data.wind.gust + "km/h</p>": "" }
-                    </div>
-                    <div class="rounded-full bg-stone-600 size-20">
-                        <svg class="h-16 mx-auto mt-2" style="rotate:${180+data.wind.deg}deg;" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 40 210" preserveAspectRatio="xMidYMid meet" >
-                            <g fill="none" stroke="orange" stroke-width="5" stroke-linejoin="round">
-                                <path d="M 20 10 V 100" stroke-dasharray="5,6"></path>
-                                <path d="M 20 100 V 200"></path>
-                                <path d="M 0 40 L 20 10 L 40 40"></path>
-                                <path d="M 0 180 L 20 150 L 40 180"></path>
-                                <path d="M 0 200 L 20 170 L 40 200"></path>
-                            </g>
-                        </svg>
-                    </div>
-                </div>
-                <hr class="my-3">
-                <div class="flex flex-row justify-between">
-                    <p><img class="inline" src="./include/icons/weather/sunrise.png"/> ${ (new Date(data.sys.sunrise*1000)).toLocaleString('en-us', {timeStyle: "short"}) }</p>
-                    <p><img class="inline" src="./include/icons/weather/sunset.png"/> ${ (new Date(data.sys.sunset*1000)).toLocaleString('en-us', {timeStyle: "short"}) }</p>
-                </div>
-                <button id="update-location" class="w-full bg-slate-500 active:bg-slate-700 rounded-lg mt-4 py-3">Update Location</button>
-            </div>`;
+            <div class="flex flex-row justify-between">
+                <p><img class="inline" src="./include/icons/weather/sunrise.png"/> ${ (new Date(data.sys.sunrise*1000)).toLocaleString('en-us', {timeStyle: "short"}) }</p>
+                <p><img class="inline" src="./include/icons/weather/sunset.png"/> ${ (new Date(data.sys.sunset*1000)).toLocaleString('en-us', {timeStyle: "short"}) }</p>
+            </div>
+            <button id="update-location" class="w-full bg-slate-500 active:bg-slate-700 rounded-lg mt-4 py-3">Update Location</button>
+        </div>`;
 
-            // Add to document
-            const weather = document.getElementById("weather");
-            weather.innerHTML = weather_content;
+        // Add to document
+        const weather = document.getElementById("weather");
+        weather.innerHTML = weather_content;
 
-            // Register event listener to toggle 'weather-extra'
-            // Prefer "onclick" to "addEventlistener" to ensure only one callback is ever registered
-            // Necessary when calling this function body multiple times
-            weather.onclick = () => {
-                // This element may not exist when we try to update our location
-                try { document.getElementById("weather-extra").classList.toggle("hidden"); }
-                catch (TypeError) {};
-            };
-            // Trigger the location reset function
-            document.getElementById("update-location").onclick = (event) => {
-                event.stopPropagation();        // Don't trigger weathermenu toggle
-                updateCoordinates();
-            };
+        // Register event listener to toggle 'weather-extra'
+        // Prefer "onclick" to "addEventlistener" to ensure only one callback is ever registered
+        // Necessary when calling this function body multiple times
+        weather.onclick = () => {
+            // This element may not exist when we try to update our location
+            try { document.getElementById("weather-extra").classList.toggle("hidden"); }
+            catch (TypeError) {};
+        };
+        // Trigger the location reset function
+        document.getElementById("update-location").onclick = (event) => {
+            event.stopPropagation();        // Don't trigger weathermenu toggle
+            updateCoordinates();
+        };
 
-        })
-        .catch(error => {
-            alert("Something broke. Check console");
-            console.log(error);
-            
-            // Set error content display
-            document.getElementById("weather").innerHTML = `
-                <img class="size-12 inline" src="./include/icons/error.png">
-                <p class="inline">Failed to load weather data.</p>
-            `;
-        });
-    };
-    
+    })
+    .catch(error => {
+        alert("Something broke. Check console");
+        console.log(error);
+        
+        // Set error content display
+        document.getElementById("weather").innerHTML = `
+            <img class="size-12 inline" src="./include/icons/error.png">
+            <p class="inline">Failed to load weather data.</p>
+        `;
+    });    
 };
 
 // Get coordinate data for weather API call.
-// Save to localStorage for future use
 function getCoordinates(){
-    
-    // Method to manually get location data
-    function getManualCoordinates(header_message){
-        const weather_tab = document.getElementById("weather");
 
-        // Construct a form to receive manual inputs from user
-        const c_form = document.createElement("form");
+    return new Promise((resolve) => {
 
-        c_form.innerHTML = `
-            <label class="text-sm" for="latitude">Latitude</label>
-            <input class="text-black rounded-md text-sm mb-2"
-                type="number" id="latitude" min="-90" max="90" step="0.01" required>
-            <br>
-            <label class="text-sm" for="longitude">Longitude</label>
-            <input class="text-black rounded-md text-sm mb-2"
-                type="number" id="longitude" min="-180" max="180" step="0.01" required>
-            <br>
-            <button class="rounded-lg bg-gray-800 p-2" type="submit">Get Weather Forecast</button>
-        `;
+        // Method to manually get location data
+        function getManualCoordinates(header_message){
+            const weather_tab = document.getElementById("weather");
 
-        // Use "header_message" to explain why we are asking for a manual input
-        weather_tab.innerHTML = `
-            <p>${header_message}</p>
-            <hr class="my-3">
-            <p text-sm>Manually get weather forecast:</p>
-        `;
-        weather_tab.append(c_form);
+            // Construct a form to receive manual inputs from user
+            const c_form = document.createElement("form");
+            c_form.innerHTML = `
+                <label class="text-sm" for="latitude">Latitude</label>
+                <input class="text-black rounded-md text-sm mb-2"
+                    type="number" id="latitude" min="-90" max="90" step="0.01" required>
+                <br>
+                <label class="text-sm" for="longitude">Longitude</label>
+                <input class="text-black rounded-md text-sm mb-2"
+                    type="number" id="longitude" min="-180" max="180" step="0.01" required>
+                <br>
+                <button class="rounded-lg bg-gray-800 p-2" type="submit">Get Weather Forecast</button>`;
 
-        // Process user inputs
-        c_form.addEventListener("submit", (event) =>{
-            event.preventDefault();
-            
-            // Set loading image while we wait for next actions
+            // Use "header_message" to explain why we are asking for a manual input
             weather_tab.innerHTML = `
-                <img class="size-12 inline" src="./include/icons/loading-transparent-bg.gif">
-                <p class="inline">Loading weather data...</p>
-            `;
+                <p>${header_message}</p>
+                <hr class="my-3">
+                <p text-sm>Manually get weather forecast:</p>`;
 
-            // Save data to localStorage
-            localStorage.setItem(gcsKey, JSON.stringify(
-                {
+            // Add form
+            weather_tab.append(c_form);
+
+            // Process user inputs
+            c_form.addEventListener("submit", (event) =>{
+                event.preventDefault();
+
+                // Set loading image while we wait for next actions
+                weather_tab.innerHTML = `
+                    <img class="size-12 inline" src="./include/icons/loading-transparent-bg.gif">
+                    <p class="inline">Loading weather data...</p>`;
+
+                const coordinates = {
                     latitude:  c_form.elements["latitude"].value,
                     longitude: c_form.elements["longitude"].value
-                }
-            ));
+                };
 
-            // Schedule updates
-            scheduleWeather();
-        });
-    };
+                // Save data to localStorage for future use
+                localStorage.setItem(gcsKey, JSON.stringify(coordinates));
 
-    // Does browser support Geolocation?
-    // If yes, we can automatically retrieve position coordinates for weather data
-    // On failure cases, fall back to manual input of coordinates to get weather data
-    if (navigator.geolocation){
+                // Resolve Promise
+                resolve(coordinates);
+            });
+        };
 
-        // Set loading image while we wait for next actions
-        document.getElementById("weather").innerHTML = `
-            <img class="size-12 inline" src="./include/icons/loading-transparent-bg.gif">
-            <p class="inline">Loading weather data...</p>
-        `;
 
-        // Attempt automatic weather retrieval
-        navigator.geolocation.getCurrentPosition(
-            success => {
-                // Save data to localStorage
-                localStorage.setItem(gcsKey, JSON.stringify(
-                    {
+        // First try to retrieve coordinates from localStorage
+        if ( !(localStorage.getItem(gcsKey) === null) ){
+            // Stop function execution via 'return' and resolve Promise
+            return resolve(JSON.parse(localStorage.getItem(gcsKey)));
+        };
+
+
+        // If browser supports Geolocation, we can automatically retrieve position coordinates for weather data
+        // using Google's location data for this user/machine.
+        // On failure cases, fall back to manual input of coordinates to get weather data.
+        if (navigator.geolocation){
+
+            // Set loading image while we wait for next actions - it may take a while
+            document.getElementById("weather").innerHTML = `
+                <img class="size-12 inline" src="./include/icons/loading-transparent-bg.gif">
+                <p class="inline">Loading weather data...</p>`;
+
+                // Attempt automatic weather retrieval
+            navigator.geolocation.getCurrentPosition(
+                success => {
+                    const coordinates = {
                         latitude:  success.coords.latitude,
                         longitude: success.coords.longitude
-                    }
-                ));
-                
-                // Schedule updates
-                scheduleWeather();
-            },
-            error => {
-                // Attempt manual input
-                getManualCoordinates(error.message);
-            }
-        );
+                    };
 
-    } else {
-        // Attempt manual input
-        getManualCoordinates("Browser does not support GeoLocation service");
-    };
+                    // Save data to localStorage for future use
+                    localStorage.setItem(gcsKey, JSON.stringify(coordinates));
+
+                    // resolve Promise
+                    resolve(coordinates);
+                },
+                error => {
+                    // Attempt manual input
+                    getManualCoordinates(error.message);
+                }
+            );
+        } else {
+            // Attempt manual input
+            getManualCoordinates("Browser does not support GeoLocation service");
+        };
+    });
 };
 
 // Clear saved coordinates and update
 function updateCoordinates(){
-    // Cancel scheduled interval, if any
-    clearInterval(weatherTask);
-    
-    // Clear data from localStorage
-    localStorage.removeItem(gcsKey);
-
-    // Restart weather loop
-    getCoordinates();
+    clearInterval(weatherTask);         // Cancel scheduled interval
+    localStorage.removeItem(gcsKey);    // Clear data from localStorage
+    scheduleWeather();                  // Restart weather loop
 };
 
 
@@ -393,11 +388,8 @@ timeTask = setInterval(() => { updateTime(); }, oneMinute);
 updateMarket();
 marketTask = setInterval(() => { updateMarket(); }, oneHour);
 
+// Function to restart this process for weather, because we may update our position.
 function scheduleWeather(){
-    /* Because waiting for coordinates can take an uncertain, possibly unlimited amount of time
-       it is best to trigger this via a function call that is embedded within the callbacks of
-       the coordinate return functions. */
-    clearInterval(weatherTask);  // if any existed before this
     updateWeather();
     weatherTask = setInterval(() => { updateWeather(); }, oneHour);
 };
